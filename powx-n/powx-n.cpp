1fd654d481385be3b3de300f31a8cd5b289c099e
amit1197 <amitkumar.chottu58@gmail.com>
2023-07-29 21:28:40 +0530

    Time: 3 ms, Memory: 5.9 MB - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     double myPow(double x, int n) {
         return helper(x,(long long)n);
-        if(n==0) return 1.0;
-        if(n<0) return 1.0/myPow(x,-n);
-        else {
-            if(n%2){
-                return myPow(x*x,n/2);
-            }
-            else{
-                return x*myPow(x*x,(n-1)/2);
-            }
-        }
+        // if(n==0) return 1.0;
+        // if(n<0) return 1.0/myPow(x,-n);
+        // else {
+        //     if(n%2){
+        //         return myPow(x*x,n/2);
+        //     }
+        //     else{
+        //         return x*myPow(x*x,(n-1)/2);
+        //     }
+        // }
     }
     double helper(double x, long long n) {
         if(n==0) return 1.0;
