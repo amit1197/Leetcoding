ad89d9e80fe061568183da76c6980fe85a273975
amit1197 <amitkumar.chottu58@gmail.com>
2023-08-06 14:49:12 +0530

    Time: 112 ms (18.69%), Space: 44.6 MB (18.89%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        vector<vector<unsigned long long > > v(s.size(),vector<unsigned long long > (t.size()));
-        if(s[0]==t[0]) v[0][0]=1;
-        else v[0][0]=0;
-        for(int i=1;i<t.size();i++) v[0][i]=0;
-        for(int i=1;i<s.size();i++) 
-        {
-            if(s[i]==t[0]) v[i][0]=v[i-1][0]+1;
-            else v[i][0]=v[i-1][0];
+        vector<vector<pair<unsigned long long,unsigned long long>>> dp(s.size()+1,vector<pair<unsigned long long,unsigned long long>>(t.size()+1, make_pair(0,1)));
+        for(int i=0;i<dp.size();i++){
+            dp[i][0]=make_pair(0,1);
         }
-        for(int i=1;i<s.size();i++)
-        {
-            for(int j=1;j<t.size();j++)
-            {
-                if(s[i]==t[j]) v[i][j] = v[i-1][j-1] + v[i-1][j];
-                else v[i][j]=v[i-1][j];
+        for(int i=0;i<dp[0].size();i++){
+            dp[0][i]=make_pair(0,1);
+        }
+        for(int i=1;i<dp.size();i++){
+            for(int j=1;j<dp[0].size();j++){
+                if(s[i-1]==t[j-1]){
+                    unsigned long long x=0;
+                    unsigned long long y=0;
+                    unsigned long long xa=dp[i-1][j-1].first+1;
+                    unsigned long long ya=dp[i-1][j-1].second;
+                    unsigned long long xb=dp[i-1][j].first;
+                    unsigned long long yb=dp[i-1][j].second;
+                    unsigned long long xc=dp[i][j-1].first;
+                    unsigned long long yc=dp[i][j-1].second;
+                    x=max(max(xa,xb),xc);
+                    if(xa==x) y+=ya;
+                    if(xb==x) y+=yb;
+                    if(xc==x) y+=yc;
+                    dp[i][j]=make_pair(x,y);
+                }
+                else{
+                    unsigned long long x=0;
+                    unsigned long long y=0;
+                    unsigned long long xb=dp[i-1][j].first;
+                    unsigned long long yb=dp[i-1][j].second;
+                    unsigned long long xc=dp[i][j-1].first;
+                    unsigned long long yc=dp[i][j-1].second;
+                    x=max(xb,xc);
+                    if(xb>xc){
+                        y=yb;
+                    }
+                    else if(xb<xc){
+                        y=yc;
+                    }
+                    else{
+                        y=max(yb,yc);
+                    }
+                    dp[i][j]=make_pair(x,y);
+
+                }
             }
         }
-        
-        return v[s.size()-1][t.size()-1];
+        // cout<<dp[s.size()][t.size()].second<<" "<<dp[s.size()][t.size()].first;
+        return dp[s.size()][t.size()].first<t.size()?0:dp[s.size()][t.size()].second;
     }
 };
