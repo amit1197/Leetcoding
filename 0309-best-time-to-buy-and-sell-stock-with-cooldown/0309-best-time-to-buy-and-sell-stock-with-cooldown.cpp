63949d8a7220795826d36338208e603d587dd9fb
amit1197 <amitkumar.chottu58@gmail.com>
2023-08-16 19:57:58 +0530

    Time: 0 ms (100%), Space: 11.3 MB (80.39%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -8,7 +8,7 @@ public:
         // base case
         s0[0]=0;
         s1[0]=-1*prices[0];
-        s2[0]=INT_MIN;
+        s2[0]=0;
         for(int i=1;i<n;i++){
             s0[i]=max(s0[i-1],s2[i-1]);
             s1[i]=max(s1[i-1],s0[i-1]-prices[i]);
