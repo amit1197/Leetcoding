aa0aa37598e364d37870d881a5d293bde5d3baea
amit1197 <amitkumar.chottu58@gmail.com>
2023-09-13 13:00:51 +0530

    Time: 0 ms (100%), Space: 40.1 MB (77.5%) - LeetHub

---
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode xParent = null;
+    TreeNode yParent = null;
+    int xDepth = 0;
+    int yDepth = 0;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        getDepthAndParent(root,x,y,0,null);
+        return xDepth==yDepth && xParent!=yParent ? true : false;
+    }
+    public void getDepthAndParent(TreeNode root,int x,int y,int depth,TreeNode parent){
+        if(root==null){
+            return;
+        }
+        if(root.val==x){
+            xParent=parent;
+            xDepth=depth;
+        }
+        else if(root.val==y){
+            yParent=parent;
+            yDepth=depth;
+        }
+        getDepthAndParent(root.left,x,y,depth+1,root);
+        getDepthAndParent(root.right,x,y,depth+1,root);
+
+    }
+}
